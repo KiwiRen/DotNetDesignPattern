90124b366cc6d6ddecf79559fd3954abf8c43e93
KiwiRen <reclyan@gmail.com>
2015-05-04 17:15:40 +0800

    Add Factory Method to the repository
    Factory Method:
    
    1. user don't need to know the name of the product
    2. user can create the instance of the product by selecting the specific
    factory.
    3. factory is reponsible for returning a instance

---
+++ 2. FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the product instance by choosing the specific factory and user don't need to know the name of product
+            IFactory factory = new FactoryA();
+            AbstractProduct productA = factory.createInstance();
+            productA.showMessage();
+
+            factory = new FactoryB();
+            AbstractProduct productB = factory.createInstance();
+            productB.showMessage();
+
+            Console.Read();
+        }
+    }
+
+    public interface IFactory
+    {
+        AbstractProduct createInstance();
+    }
+
+    public abstract class AbstractProduct
+    {
+        public virtual void showMessage()
+        {
+            Console.WriteLine("This is abstract product");
+        }
+    }
+
+    public class ProductA : AbstractProduct
+    {
+        public override void showMessage()
+        {
+            Console.WriteLine("This is productA");
+        }
+    }
+
+    public class ProductB : AbstractProduct
+    {
+        public override void showMessage()
+        {
+            Console.WriteLine("This is productB");
+        }
+    }
+
+    public class FactoryA : IFactory
+    {
+        public AbstractProduct createInstance()
+        {
+            AbstractProduct product = new ProductA();
+            return product;
+        }
+    }
+
+    public class FactoryB : IFactory
+    {
+        public AbstractProduct createInstance()
+        {
+            AbstractProduct product = new ProductB();
+            return product;
+        }
+    }
+}
