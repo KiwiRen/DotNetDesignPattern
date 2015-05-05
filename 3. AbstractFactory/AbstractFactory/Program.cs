7f5e6925e4c8a2ec4139a561e1e4456984981830
KiwiRen <reclyan@gmail.com>
2015-05-05 09:40:03 +0800

    Add Abstract Factory
    1. This pattern is used for creating instance for group
    2. Each factory here manages the creation of one group

---
+++ 3. AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IFactory factoryFamilyA = new FactoryFamilyA();
+            ProductA productAFamilyA = factoryFamilyA.getProductAInstance();
+            ProductB productBFamilyA = factoryFamilyA.getProductBInstance();
+
+            productAFamilyA.showMessage();
+            productBFamilyA.showMessage();
+
+            IFactory factoryFamilyB = new FactoryFamilyB();
+            ProductA productAFamilyB= factoryFamilyB.getProductAInstance();
+            ProductB productBFamilyB = factoryFamilyB.getProductBInstance();
+
+            productAFamilyB.showMessage();
+            productBFamilyB.showMessage();
+
+            Console.Read();
+        }
+    }
+
+    public abstract class ProductA
+    {
+        public virtual void showMessage()
+        {
+            Console.WriteLine("This is the base class of Product A");
+        }
+    }
+
+     public abstract class ProductB
+    {
+        public virtual void showMessage()
+        {
+            Console.WriteLine("This is the base class of Product B");
+        }
+    }
+
+    public class ProductAFamilyA : ProductA
+    {
+        public override void showMessage()
+        {
+            Console.WriteLine("This is Product A in Family A");
+        }
+    }
+
+    public class ProductBFamilyA : ProductB
+    {
+        public override void showMessage()
+        {
+            Console.WriteLine("This is Product B in Family A");
+        }
+    }
+
+    public class ProductAFamilyB : ProductA
+    {
+        public override void showMessage()
+        {
+            Console.WriteLine("This is Product A in Family B");
+        }
+    }
+
+    public class ProductBFamilyB : ProductB
+    {
+        public override void showMessage()
+        {
+            Console.WriteLine("This is Product B in Family B");
+        }
+    }
+
+    public interface IFactory
+    {
+        ProductA getProductAInstance();
+        ProductB getProductBInstance();
+    }
+
+    public class FactoryFamilyA : IFactory
+    {
+        public ProductA getProductAInstance()
+        { 
+            ProductA product = new ProductAFamilyA();
+            return product;
+        }
+
+        public ProductB getProductBInstance()
+        {
+            ProductB product = new ProductBFamilyA();
+            return product;
+        }
+
+    }
+
+    public class FactoryFamilyB : IFactory
+    {
+        public ProductA getProductAInstance()
+        {
+            ProductA product = new ProductAFamilyB();
+            return product;
+        }
+
+        public ProductB getProductBInstance()
+        {
+            ProductB product = new ProductBFamilyB();
+            return product;
+        }
+
+    }
+}
