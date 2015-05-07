350c48deb31e89d4f54de817aa9b18d41c6a5777
KiwiRen <reclyan@gmail.com>
2015-05-07 17:30:57 +0800

    Add Builder Pattern
    This pattern is used for generating a complex object
    
    1. Director is regarded as a factory to control the flow
    2. Different concreate builder have the same behavoir to generate the
    seperated parts of the comple product

---
+++ 5. Builder/Builder/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director(new BuilderA());
+            director.createProduct();
+
+            director = new Director(new BuilderB());
+            director.createProduct();
+
+            Console.Read();
+
+        }
+    }
+
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+    }
+
+    public class BuilderA : IBuilder
+    {
+        public void BuildPartA()
+        {
+            Console.WriteLine("Builder A build the partA");
+        }
+
+        public void BuildPartB()
+        {
+            Console.WriteLine("Builder A build the partB");
+        }
+    }
+
+    public class BuilderB : IBuilder
+    {
+        public void BuildPartA()
+        {
+            Console.WriteLine("Builder B build the partA");
+        }
+
+        public void BuildPartB()
+        {
+            Console.WriteLine("Builder B build the partB");
+        }
+    }
+
+    public class Director
+    {
+        private IBuilder builder;
+
+        public Director(IBuilder builder)
+        {
+            this.builder = builder;
+        }
+
+        public void createProduct()
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+        }
+    
+    }
+}
