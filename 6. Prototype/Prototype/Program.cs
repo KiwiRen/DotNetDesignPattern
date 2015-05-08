f93f4e75a5058ec905230617ab6e03808d866291
KiwiRen <reclyan@gmail.com>
2015-05-08 11:18:22 +0800

    Add Prototype Pattern
    1. Prototype is used for generating the similar item in a real fast way.
    2. In c#, an interface IClonable can be derived to implement the Clone()
    method
    3. Two types of clone exists: deep clone and shadow clone
    4. deep clone generates two different objects, the objects can be
    affected each other
    5. Shadow clone generates two different object name which pointed to the
    same  memory. If we the value of the member of the object, another one
    will be affected.

---
+++ 6. Prototype/Prototype/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreatePrototype objectA = new ConcreatePrototype();
+            objectA.state = "A: Processing";
+            //This clone is deep clone 
+            ConcreatePrototype objectB = (ConcreatePrototype)objectA.Clone();
+            Console.WriteLine(objectB.state);
+            //the change of Object b will not affect objectA
+            objectB.state = "B: Pending";
+            Console.WriteLine(objectB.state);
+            Console.WriteLine(objectA.state);
+            //Shadow Copy
+            ConcreatePrototype objectC = objectA;
+            Console.WriteLine(objectC.state);
+            objectC.state = "C:Pending";
+            //the change of objectC will affect objectA
+            Console.WriteLine(objectC.state);
+            Console.WriteLine(objectA.state);
+            Console.Read();
+
+       
+        }
+    }
+
+    public class ConcreatePrototype : ICloneable
+    {
+        private string _state;
+        public string state
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public ConcreatePrototype()
+        {
+            state = "Initialize";
+        }
+
+        public object Clone()
+        { 
+            ConcreatePrototype prototype = new ConcreatePrototype();
+            prototype.state = this.state;
+
+            return prototype;
+        }
+    }
+
+
+}
