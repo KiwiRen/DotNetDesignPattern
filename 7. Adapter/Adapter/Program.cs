947cea73f6023987646db0b31c309ce607a31885
KiwiRen <reclyan@gmail.com>
2015-05-11 17:51:04 +0800

    Add Adapter Pattern
    This pattern is used for tranlating some old methods to a new interface
    or principle
    1. Adapter takes the responsibility to convert the methods in adaptee to
    meet the expectation of new interface ITarget

---
+++ 7. Adapter/Adapter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget objectA = new NormalTarget();
+            objectA.ProcessRequest();
+            //Adapter class is used to convert the old method to new specification
+            ITarget objectB = new Adapter();
+            objectB.ProcessRequest();
+
+            Console.Read();
+        }
+    }
+
+    public interface ITarget
+    {
+        void ProcessRequest();
+    }
+
+    public class NormalTarget:ITarget
+    {
+
+        public void ProcessRequest()
+        {
+            Console.WriteLine("This is used to process normal request");
+            
+        }
+    }
+
+    public class Adapter :Adaptee,ITarget
+    {
+        public void ProcessRequest()
+        {
+            base.ProcessOldRequest();
+        }
+    }
+
+    public class Adaptee
+    {
+        public void ProcessOldRequest()
+        {
+            Console.WriteLine("This is used to process old request");
+
+        }  
+    }
+}
