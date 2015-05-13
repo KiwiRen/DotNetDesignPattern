b4adcee972e09a0ca5ed37cd7f3f45685459f3f6
KiwiRen <reclyan@gmail.com>
2015-05-13 17:07:34 +0800

    Add Proxy Pattern

---
+++ 9. Proxy/Proxy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISubject subject = new Proxy();
+
+            subject.ProcessResponse();
+
+            Console.Read();
+        }
+    }
+
+    public interface ISubject
+    {
+        void ProcessResponse();
+    }
+
+    public class Subject:ISubject
+    {
+        public void ProcessResponse()
+        {
+            Console.WriteLine("Process Response");
+        }
+    }
+
+    public class Proxy :ISubject
+    {
+        private Subject subject = new Subject();
+
+        public void ProcessResponse()
+        {
+            Console.WriteLine("Pre-Process Response");
+            subject.ProcessResponse();
+            Console.WriteLine("Finish-Process Response");
+        
+        }
+    
+    }
+}
