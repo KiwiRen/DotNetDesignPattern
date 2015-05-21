a3083621289d338d90e712cf59330141da73d9c8
KiwiRen <reclyan@gmail.com>
2015-05-21 15:33:08 +0800

    Add Facade Pattern

---
+++ 10. Facade/Facade/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.display();
+            Console.Read();
+        }
+    }
+
+    public class Facade
+    {
+        public void display()
+        {
+            FunctionalityA fa = new FunctionalityA();
+            FunctionalityB fb = new FunctionalityB();
+
+            fa.display();
+            fb.display();
+        }
+    }
+
+    public class FunctionalityA
+    {
+        public void display()
+        {
+            Console.WriteLine("This is A");
+        }
+    }
+
+    public class FunctionalityB
+    { 
+        public void display()
+        {
+            Console.WriteLine("This is B");
+        }
+    }
+}
